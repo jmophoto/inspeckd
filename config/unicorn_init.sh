7bbb6f66abc23da2e6add12d6d7bb1218340ab72
jmophoto <jason@jmophoto.com>
2014-02-22 13:27:19 -0700

    changed the APP_ROOT in unicorn_init.sh

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -3,8 +3,9 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
+
 APP_ROOT=/home/inspeckd/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
+PID=/home/unicorn/pids/inspeckd.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=root
 set -u
